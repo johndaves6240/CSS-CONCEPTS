835fd12d384da70a3e5077404bcff46f01fc4322
johndaves6240 <johndaves6240@gmail.com>
2024-02-12 13:43:31 +0800

    adding the files

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeName(){
+    // if ID "." if class
+    // var element = document.querySelector('#parag');
+    // if (element) {
+    //     element.innerHTML = "test123";
+    // }
+
+    var element = document.querySelector('p').innerHTML;
+    console.log(element);
+}
